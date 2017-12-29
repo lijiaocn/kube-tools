7061cb55738c4d11b448fcd5244a67a5d5efbc06
lijiaocn <lijiaocn@foxmail.com>
2017-12-29 18:10:26 +0800

    check tunnel ip leak

--- calico-ip-conflict/calico.ipip.tunnel.ip.sh
+++ calico-ip-conflict/calico.ipip.tunnel.ip.sh
@@ -3,5 +3,5 @@
 for host in `etcdctl ls /calico/v1/host`
 do
 	tunnel_ip=`etcdctl get "$host/config/IpInIpTunnelAddr"`
-	echo -e "$tunnel_ip\t$host"
+	echo -e "$tunnel_ip/32\t$host"
 done
--- calico-ip-conflict/run.sh
+++ calico-ip-conflict/run.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 IP_USAGE=ip.calico.usage.dat
 calicoctl get workloadendpoint -o wide >$IP_USAGE
+
 IP_UNALLOC=ip.unallocate.dat
 ./calico.all.unallocate.ip.sh >$IP_UNALLOC
+
 IP_RETURN=ip.need.return.dat
 grep -f $IP_UNALLOC $IP_USAGE >$IP_RETURN
 cat $IP_RETURN |awk '{ print $3 }' |awk --field-separator="." '{ print "kubectl -n "$1" delete pod "$2 }' >pod.rebuild.sh
+
 HOST_TUNNEL=host.tunnel.ip.dat
 ./calico.ipip.tunnel.ip.sh >$HOST_TUNNEL
-grep -f $IP_UNALLOC $HOST_TUNNEL >tunnel.ip.need.return.dat
+
+IP_LEAK=tunnel.ip.leak.dat
+grep -f $IP_UNALLOC $HOST_TUNNEL >$IP_LEAK
+
+TUNNEL_IP_RETURN=tunnel.ip.need.return.dat
+grep -f $HOST_TUNNEL $IP_USAGE > $TUNNEL_IP_RETURN
+cat $TUNNEL_IP_RETURN |awk '{ print $3 }' |awk --field-separator="." '{ print "kubectl -n "$1" delete pod "$2 }' >>pod.rebuild.sh
+
+# bash ./pod.rebuild.sh
