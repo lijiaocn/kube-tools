7577e4a8a018f3784347051a0ac164e4d321c3a1
lijiaocn <lijiaocn@foxmail.com>
2018-01-04 12:59:13 +0800

    fix

--- calico-ip-conflict/run.sh
+++ calico-ip-conflict/run.sh
@@ -10,10 +10,7 @@ grep -f $IP_UNALLOC $IP_USAGE >$IP_RETURN
 cat $IP_RETURN |awk '{ print $3 }' |awk --field-separator="." '{ print "kubectl -n "$1" delete pod "$2 }' >pod.rebuild.sh
 
 HOST_TUNNEL=host.tunnel.ip.dat
-./calico.ipip.tunnel.ip.sh >$HOST_TUNNEL
-
-IP_LEAK=tunnel.ip.leak.dat
-grep -f $IP_UNALLOC $HOST_TUNNEL >$IP_LEAK
+./calico.ipip.tunnel.ip.sh | awk '{print $1}' >$HOST_TUNNEL
 
 TUNNEL_IP_RETURN=tunnel.ip.need.return.dat
 grep -f $HOST_TUNNEL $IP_USAGE > $TUNNEL_IP_RETURN
