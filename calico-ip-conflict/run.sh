538fa83d16d1f16c2aadb2fb02bce29c4bf217e3
lijiaocn <lijiaocn@foxmail.com>
2018-01-08 12:16:46 +0800

    detect tunnleip

--- calico-ip-conflict/run.sh
+++ calico-ip-conflict/run.sh
@@ -12,8 +12,12 @@ cat $IP_RETURN |awk '{ print $3 }' |awk --field-separator="." '{ print "kubectl 
 HOST_TUNNEL=host.tunnel.ip.dat
 ./calico.ipip.tunnel.ip.sh | awk '{print $1}' >$HOST_TUNNEL
 
+TUNNEL_IP_UN_RECORD=tunnel.ip.need.record.dat
+grep -f $HOST_TUNNEL $IP_UNALLOC >$TUNNEL_IP_UN_RECORD
+
 TUNNEL_IP_RETURN=tunnel.ip.need.return.dat
 grep -f $HOST_TUNNEL $IP_USAGE > $TUNNEL_IP_RETURN
 cat $TUNNEL_IP_RETURN |awk '{ print $3 }' |awk --field-separator="." '{ print "kubectl -n "$1" delete pod "$2 }' >>pod.rebuild.sh
 
-# bash ./pod.rebuild.sh
+echo 'echo "finished"' >>pod.rebuild.sh
+#bash ./pod.rebuild.sh
