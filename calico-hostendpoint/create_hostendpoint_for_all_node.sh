57003d0f62f58e091a71de8e887a036205dd461a
lijiaocn <lijiaocn@foxmail.com>
2018-01-19 16:14:22 +0800

    fix: get node name

--- calico-hostendpoint/create_hostendpoint_for_all_node.sh
+++ calico-hostendpoint/create_hostendpoint_for_all_node.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for node in `kubectl get node  -o go-template="{{ range .items }} {{ index .status.addresses 1 \"address\" }} {{end}}"`;do
+#for node in `kubectl get node  -o go-template="{{ range .items }} {{ index .status.addresses 1 \"address\" }} {{end}}"`;do
+for node in `kubectl get node |awk '{ if(NR>1)print $1}' `;do
     ip=`kubectl get node $node -o go-template="{{ index .status.addresses 0 \"address\" }}"`
     tunnel=`./get_host_tunnel_ip.sh $node`
     echo $node $ip $tunnel
